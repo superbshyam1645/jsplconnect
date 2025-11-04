9a86722589db2688b6679af4d00703d08a3c319d
cubastionfrontend1645 <cubastion.1234@gmail.com>
2025-11-04 13:18:33 +0530

    Initial commit

--- server/index.ts
+++ server/index.ts
@@ -70,12 +70,8 @@ app.use((req, res, next) => {
   // Other ports are firewalled. Default to 5000 if not specified.
   // this serves both the API and the client.
   // It is the only port that is not firewalled.
-  const port = parseInt(process.env.PORT || '5000', 10);
-  server.listen({
-    port,
-    host: "0.0.0.0",
-    reusePort: true,
-  }, () => {
-    log(`serving on port ${port}`);
-  });
+const port = process.env.PORT ? parseInt(process.env.PORT, 10) : 3000;
+server.listen(port, () => {
+  log(`serving on port ${port}`);
+});
 })();
